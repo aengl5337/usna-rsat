9059fe2110499da2d53579ea4a90b1bd00eaf88f
aengl5337 <59488176+aengl5337@users.noreply.github.com>
2023-10-12 23:11:38 -0500

    Update forwardKin.m
    blanket update

--- robotics/archive/manual/forwardKin.m
+++ robotics/archive/manual/forwardKin.m
@@ -1,56 +1,78 @@
-function [T0_6,T] = forwardKin(thetas, D_H) % This works in its current form, but needs serious convention rehaul
-    
-    nMotors = numel(thetas)-1;
-    
-    % GIVENS    
-    a = D_H(:,1);
-    d = D_H(:,2);
-    alpha = D_H(:,3);
+%{
+TODO:
+() + Add standard D-H version
+() + Display option?
+() - appears fwd kinematics are negative what they should be -- why?
+%}
 
-    T=zeros(4,4,nMotors);
+
+
+function T0_i = forwardKin(q, q_types, D_H, D_H_type) 
+% INPUTS:
+%
+% q = column vector of joint variables
+% q_types = vector of of booleans indexed by joint specifying {revolute = 0, prismatic = 1}
+% D_H = Matrix of (intrinsic) Denavit-Hartenberg parameters:
+%       If standard, row i: [theta_i,d_i,alpha_i,a_i]
+%       If modified, row i: [alpha_(i-1),a_(i-1),theta_i,d_i]
+% D_H_type = string specifying Denavit-Hartenberg notation version
+% {"std","mod"}
+% 
+% OUTPUTS:
+%
+% T0_i = homogeneous basis transformation matrix betwen frame/joint {i} and {0}
+%
+
+
+    N = numel(q);
     
-    for i = 1:nMotors % From [1,7]
-        % Reverse index, from [row 7 (joint 6),row 2 (joint 1)]
-        i_rev = nMotors-(i-2);
-        display(i_rev)
-        
-        % Generate T(i_rev-2)_(i_rev-1)
-        % i.e. for i=1, i_rev=7, generating T5_6
-        %      for i=6, i_rev=2, generating T0_1
-        
-        % D_d, R_theta, and D_a formulas stay the same for every joint
-        D_a = axialTranslation(a(i_rev-1),'x');
-        D_d = axialTranslation(d(i_rev),'z');
-        R_theta = planarRotation(thetas(i_rev),'z');
-        
-        % R_alpha needs to be specified diferently when considering T0_1
-        if i_rev == 2
-            % This y here is to rotate base coordinates so they are consistent with NSTAR engineering drawings
-            R_alpha = planarRotation(alpha(i_rev-1),'y');
-        else
-            R_alpha = planarRotation(alpha(i_rev-1),'x');
+    if D_H_type=="mod"
+    
+        % Extract parameters by columns and add joint variables
+        alpha = D_H(:,1);
+        a = D_H(:,2);
+        theta = not(q_types).*q + D_H(:,3); % Adds joint variable if revolute
+        d = q_types.*q + D_H(:,4);          % Adds joint variable if prismatic
+
+        Tadj =zeros(4,4,N); % Adjacent basis transformations, (i) to (i-1)
+
+        for i = 1:N % From [1,7]
+            % Assign the overall transformation to a data cube, layer indexed
+            % by joint [from 6 to 1]
+            
+            % Emulating equation (3.4) from Craig:
+            % ^(i-1)_(i)T = R_x(alpha_i-1)D_x(a_i-1)R_z(theta_i)D_z(d_i)
+            R_alpha = planarRotation(alpha(i),'x');
+            D_a = axialTranslation(a(i),'x');
+            R_theta = planarRotation(theta(i),'z');
+            D_d = axialTranslation(d(i),'z');
+            
+%             Tadj(:,:,i) = R_alpha*D_a*R_theta*D_d;
+            Tadj(:,:,i) = [cosd(theta(i))                -sind(theta(i))                 0                a(i);                   
+                           sind(theta(i))*cosd(alpha(i))  cosd(theta(i))*cosd(alpha(i)) -sind(alpha(i))  -d(i)*sind(alpha(i));
+                           sind(theta(i))*sind(alpha(i))  cosd(theta(i))*sind(alpha(i))  cosd(alpha(i))   d(i)*cosd(alpha(i));
+                           0                              0                              0                1];
+        end
+
+        %{
+        figure(1)
+
+        hold on
+        for j=1:nMotors
+            x=r(1,j);
+            y=r(2,j);
+            z=r(3,j);
+            plot3(x,y,z,'ro')
+        end
+        %}
+        T0_i = zeros(4,4,N); % Initialize overall basis transformation matrix from {6} to {0}
+        for j=1:N % Incrementally matrix-multiply all transformations together, from joint 6 to 1
+            if j==1
+                T0_i = Tadj(:,:,j);
+            else
+                T0_i(:,:,j) = T0_i(:,:,j-1)*Tadj(:,:,j);
+            end            
         end
         
-        % Assign the overall transformation to a data cube, layer indexed
-        % by joint [from 6 to 1]
-        T(:,:,(i_rev-1)) = R_alpha*D_a*R_theta*D_d;
-        
-    end
-      
-    %{
-    figure(1)
-    
-    hold on
-    for j=1:nMotors
-        x=r(1,j);
-        y=r(2,j);
-        z=r(3,j);
-        plot3(x,y,z,'ro')
-    end
-    %}
-    T0_6 = eye(4,4); % Initialize overall transformation matrix from {6} to {0}
-    for joint=1:nMotors % Incrementally matrix-multiply all transformations together, from joint 6 to 1
-        joint_rev = nMotors-(joint-1);
-        T0_6 = T(:,:,joint_rev)*T0_6;
     end
 end
